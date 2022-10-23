ac4d288ee98011a90d25f9335215d70263d5dddd
Sergey <sergey@MacBook-Pro-Sergey.local>
2022-10-23 14:00:47 +0300

    init

---
+++ testWorkLogic/Controllers/UserController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace testWorkLogic.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+     
+
+        private readonly ILogger<User> _logger;
+
+        private readonly IConfiguration _config;
+
+        public UserController(ILogger<User> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+
+        }
+        public IDbConnection Connection
+        {
+
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("ConnectionString1"));
+            }
+        }
+
+
+        [HttpGet]
+        public List<User> Get()
+        {
+            using (IDbConnection db = Connection)
+            {
+
+                var result = db.Query<User>("SELECT * FROM Users").ToList();
+                return result;
+               
+            }
+
+         
+        }
+    }
+}
+
---
+++ testWorkLogic/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace testWorkLogic
+{
+    public class User
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Balance { get; set; }
+
+        public DateTime Created { get; set; }
+
+    }
+}
+
